02836abd65d9dcfd62d9f0ed8ad2de3acdcc4ac6
KimHyunJin <khj745700@naver.com>
2025-02-27 03:29:50 +0900

    test[g]: 패스워드 검사 모두 충족

---
+++ src/main/java/password/PasswordStrengthMeter.java
@@ -0,0 +1,10 @@
+package password;
+
+public class PasswordStrengthMeter {
+
+    public PasswordStrengthMeter() {}
+
+    public PasswordStrength meter(String s) {
+        return PasswordStrength.STRONG;
+    }
+}
--- src/test/java/PasswordStrengthMeterTest.java
+++ src/test/java/PasswordStrengthMeterTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import password.PasswordStrength;
+import password.PasswordStrengthMeter;
 
 
 import static org.junit.jupiter.api.Assertions.*;
