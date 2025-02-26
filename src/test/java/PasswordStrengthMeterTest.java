7f84a11e4e4b0d45dce844d5b3758f8099ce248a
KimHyunJin <khj745700@naver.com>
2025-02-27 03:25:03 +0900

    test[r]: 패스워드 검사 모두 충족

---
+++ src/test/java/PasswordStrengthMeterTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PasswordStrengthMeterTest {
+
+    /**
+     * TDD 방식을 사용할 때 고려해야 할 것
+     *
+     *
+     * 1. 모든 규칙을 충족하는 경우
+     * 2. 모든 조건을 충족하지 않는 경우
+     *
+     * 1. 경우는 각각의 규칙들을 red-green-blue 방식으로 차근히 개발할 수 있음.
+     * 2. 경우는 모든 규칙들을 일단 구현해야 함.
+     *  - 규칙 n개가 있다고 했을 때, 특정 값 x가 모든 조건을 충족하지 않고, 특정 값 y또한 모든 조건을 충족하지 않았다고 하자.
+     *  - 그러면 그 조건들을 반드시 구현해야 모든 조건을 충족하지 않는다고 할 수 있다.
+     *  왜? 서로 다른 x, y의 상관관계에 대해 정의해야 하기 때문이다.
+     *
+     * 따라서 모든 규칙을 충족하는 경우부터 작성하는 것이 좋다.
+     */
+    @Test
+    void MeetsAllCriteria_Then_Strong(){
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        PasswordStrength result = meter.meter("ab12!@AB");
+        assertEquals(PasswordStrength.STRONG, result);
+    }
+}
