23366a895b92784042260a08fe9443aed12535d4
KimHyunJin <khj745700@naver.com>
2025-02-27 23:29:07 +0900

    test[green]: 첫 납부일과 만료일 일자가 다를때 10만원 납부 테스트 통과

--- src/main/java/shop/khj745700/blog/expirydate/ExpiryDateCalculator.java
+++ src/main/java/shop/khj745700/blog/expirydate/ExpiryDateCalculator.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 public class ExpiryDateCalculator {
 
     private static final int PAY_PER_MONTH = 10_000;
+    private static final int PAY_PER_YEAR = 100_000;
 
     public LocalDate calculateExpiryDate(PayData payData) {
         LocalDate expectedDate = payData.billingDate().plusMonths(getPayPerMonth(payData.payment()));
@@ -13,13 +14,16 @@ public class ExpiryDateCalculator {
         }
 
         if(expectedDate.getDayOfMonth() < payData.firstBillingDate().getDayOfMonth()) {
-            return expectedDate.withDayOfMonth(Math.min(expectedDate.getMonth().maxLength(), payData.firstBillingDate().getDayOfMonth()));
+            return expectedDate.withDayOfMonth(Math.min(expectedDate.getMonth().length(expectedDate.isLeapYear()), payData.firstBillingDate().getDayOfMonth()));
         }
         return expectedDate;
     }
 
 
     private int getPayPerMonth(int payment) {
+        if(payment >= PAY_PER_YEAR) {
+            return (payment / PAY_PER_YEAR) * 12;
+        }
         return payment / PAY_PER_MONTH;
     }
 }
